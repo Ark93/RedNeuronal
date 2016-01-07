80d52fbc51a89407e11234ed8ed43adcb8412f80
Santos Bañuelos <santos.banuelos93@gmail.com>
2016-01-06 23:27:08 -0600

    Update of notebook, reading from csv file and filtering, next to do is parse date

---
+++ RedNeuronal.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas
+
+import csv
+
+#
+
+#
+
+#to open the csv file
+with open('Consulta_Banxico.csv','rb')as csvfile :
+    datareader = csv.reader(csvfile )
+    rows = 0
+    columns
+    for row in datareader:
+        rows+=1
+
+    data = num
+    csvfile.close()
